8d7c78788019ae42e85989177516f6e6a3754735
Clesio <kornalewski@outlook.com>
2024-04-03 10:48:27 -0300

    auth user

---
+++ backend/controllers/userControle.js
@@ -0,0 +1,12 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import User from "../models/userModel.js";
+
+
+//@desc Auth user & get token
+//@route GEt /api/users/login
+//@access Public
+
+const authUser = asyncHandler (async (req, res) => {
+    
+
+})
