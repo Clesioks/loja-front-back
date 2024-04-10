c40a57d55bad878d346a8dfa01f61bfef0aa699f
Clesio <kornalewski@outlook.com>
2024-04-10 08:03:28 -0300

    server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 dotenv.config()
 import connectDB from './config/DB.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
 
 
 const port = process.env.PORT || 5000
@@ -16,6 +18,9 @@ const app = express()
 // body parser middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
+
+// Cookie parser middleware
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
     res.send('API está rodando...')
