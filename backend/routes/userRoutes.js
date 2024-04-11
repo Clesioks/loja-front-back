32a8970878e70faf677ac9214320887a56dbfe96
Clesio <kornalewski@outlook.com>
2024-04-11 19:50:24 -0300

    rotas

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,15 +8,16 @@ import { authUser,
     deleteUser,
     getUserByID,
     updateUser } from '../controllers/userController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
     const router = express.Router()
 
 
-router.route('/').post(registerUser).get(getUsers)
+router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/logout', logoutUser)
-router.post('/login', authUser)
-router.route('/profile').get(getUserProfile).put(updateUserProfile)
-router.route('/:id').delete(deleteUser).get(getUserByID).put(updateUser)
+router.post('/auth', authUser)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
+router.route('/:id').delete(admin, getUsers, deleteUser).get(admin, getUsers, getUserByID).put(admin, getUsers, updateUser)
 
 
  
