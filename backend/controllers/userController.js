99ecfe973fc9b05b2fd94b9120f975fd79b32c30
Clesio <kornalewski@outlook.com>
2024-04-13 08:52:47 -0300

    users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
-import jwt from 'jsonwebtoken'
+import generateToken from "../utils/generateToken.js";
+
 
 //@desc Auth user & get token
 //@route POST /api/users/login
@@ -12,20 +13,9 @@ const authUser = asyncHandler (async (req, res) => {
     const user = await User.findOne({ email: email })
     if (user && (await user.matchPassword(password))) {
 
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-            expiresIn: '30d'
-        })
+      generateToken(res, user._id)
 
-        // Set JWT as HTTP-Only cookie
-        res.cookie('jwt', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV !== 'development',
-            sameSite: 'strict',
-            maxAge: 30 * 24 * 60 * 60 * 1000 // 30 dias
-        })
-
-
-        res.json({
+        res.status(200).json({
             _id: user._id,
             name: user.name,
             email: user.email,
@@ -58,6 +48,7 @@ const registerUser = asyncHandler (async (req, res) => {
     })
 
     if (user) {
+        generateToken(res, user._id)
         res.status(201).json({
             _id: user._id,
             name: user.name,
@@ -88,7 +79,22 @@ const logoutUser = asyncHandler (async (req, res) => {
 //@access Private
 
 const getUserProfile = asyncHandler (async (req, res) => {
-    res.send('get user profile')
+    
+    const user = await User.findById(req.user._id)
+
+    if(user) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        })
+    } else {
+        res.status(404)
+        throw new Error('Usuário não encontrado')
+    }
+
+
 })
 
 //@desc Update user profile
@@ -96,7 +102,28 @@ const getUserProfile = asyncHandler (async (req, res) => {
 //@access Private
 
 const updateUserProfile = asyncHandler (async (req, res) => {
-    res.send('update user profile')
+    const user = await User.findById(req.user._id)
+
+    if(user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updateUser = await user.save()
+
+        res.status(200).json({
+            _id: updateUser._id,
+            name: updateUser.name,
+            email: updateUser.email,
+            isAdmin: updateUser.isAdmin
+        })
+    } else {
+        res.status(404)
+        throw new Error('Usuário não encontrado')
+    }
 })
 
 //@desc Get users
