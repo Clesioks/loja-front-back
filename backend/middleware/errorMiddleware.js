41d9fa6df149c34e48953065fdc30471b331b5af
Clesio <kornalewski@outlook.com>
2024-04-24 08:05:58 -0300

    register

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,5 +1,5 @@
 const notFound = (req, res, next) => {
-    const error = new Error (`Not Found - ${req.originaUrl}`)
+    const error = new Error (`Não encontrado - ${req.originaUrl}`)
     res.status(404)
     next(error)
 }
